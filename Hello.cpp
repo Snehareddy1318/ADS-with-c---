79f04029552997a613cd184fcd558124d4b24698
Snehareddy1318 <168469226+Snehareddy1318@users.noreply.github.com>
2025-01-02 09:54:53 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-int main()
-{
-    return 0;
+using namespace std;
+int main (int argc, char** argv) {
+    double PI = 3.14;
+    int b = (int) PI;
+    cout<< b << endl;
+    int c = static_cast <int> (PI);
+    cout <<c;
 }
